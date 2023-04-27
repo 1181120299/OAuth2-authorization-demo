2e62d2fee7ed112fd74ff7a5b68c3e66d12befa0
Jack <1181120299@qq.com>
2023-04-27 20:08:33 +0800

    修改依赖版本

--- resource-server/src/main/java/com/jack/resourceserver/controller/HomePageController.java
+++ resource-server/src/main/java/com/jack/resourceserver/controller/HomePageController.java
@@ -1,13 +1,15 @@
 package com.jack.resourceserver.controller;
 
 import com.jack.clientauthority.annotation.HomePageProvider;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class HomePageController implements HomePageProvider {
 
     @Override
-    public String homePage() {
+    public String homePage(HttpServletRequest request, HttpServletResponse response) {
         return "redirect:/user/page";
     }
 }
